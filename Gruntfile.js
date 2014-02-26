5d2853130b60fcbd24e6466a573ecc4cfacb6dec
Mauricio Ramirez <mau.ram.ro@gmail.com>
2014-02-26 14:49:32 -0600

    changed 127.0.0.1 to localhost

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
     connect: {
       server: {
         options: {
-          hostname:'127.0.0.1',
+          /*hostname:'localhost',*/
           base: 'app',
           livereload: true,
           open:true
