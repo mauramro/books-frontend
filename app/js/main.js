f5024bbe2f771ec355f84504a1f9688bbb32e5a4
Mauricio Ramirez <mau.ram.ro@gmail.com>
2014-03-10 08:33:58 -0600

    added break points

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,63 @@
-function myFunction()
-{
-var x="";
-var time=new Date().getHours();
-if (time<20)
-  {
-  x="Good day";
-  }
-else
-  {
-  x="Good evening";
-  }
-document.getElementById("demo").innerHTML=x;
-}
+/*function login(){*/
 
-function test(){
-  $.getJSON("http://books-backend.herokuapp.com/api/v1/books"), function(data) {
-    console.log(data);
-      var feed = data.feed;
-      console.log(feed);
+  function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+  }
+
+   Element.prototype.toggle = function() {
+    if ( this.style.display == '' || this.style.display == 'none' ) {
+        //alert ( "Changing display to NONE ");
+        this.style.display = 'block';
+    }else{
+        this.style.display = 'none';
+        //alert ( "Changing display to BLOCK ");
+      }
     }
-}
+
+  console.log(document.getElementsByTagName("button")[0]);
+  document.getElementsByTagName("button")[0].onclick = function(event){
+  event.preventDefault();
+  //console.log("test");
+
+
+
+  var container = document.getElementsByClassName("container")[1];
+  var login = document.getElementById("test");
+
+  if(hasClass(container, 'closed') == true){
+    container.className = '';
+    container.className = 'container , open';
+    login.toggle();
+    //alert("clos");
+  }else{
+    container.className = 'container , closed';
+    login.toggle();
+  }
+
+
+  /*if(container.hasClass() = "closed"){
+    alert("test");
+  }*/
+
+  //document.getElementById("#test").slideToggle();
+ /* if(container.hasClass(container, "closed")){
+    $(container).removeClass(container, "closed" );
+    $(container).addClass(container, "open");
+  } else {
+    $(container).removeClass(container, "open");
+      $(container).addClass(container, 'closed');
+  }*/
+};
+
+/*$(document).mouseup(function (e)
+{
+    var container = $("#test");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.slideUp();
+        $(".container").addClass('closed');
+    }
+});*/
+/*}*/
